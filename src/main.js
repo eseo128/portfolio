b57e916d8fbb984538948d31abde9c510f03ff0f
eseo128 <34049770+eseo128@users.noreply.github.com>
2021-06-14 23:59:18 +0900

    JS: Project Filltering

--- src/main.js
+++ src/main.js
@@ -57,6 +57,31 @@ arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 })
 
+//Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__project');
+const project = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+
+        projectContainer.classList.add('anim-out');
+
+    setTimeout(() => {
+          project.forEach((project) => {
+        console.log(project.dataset.type);
+        if (filter === '*' || filter === project.dataset.type) {
+            project.classList.remove('invisible');
+        } else {
+            project.classList.add('invisible');
+        }
+    });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+    });
+
 
 // 유틸리티 함수: 스크롤링 펑션으로 간단하게 주기
 function scrollIntoView(selector) {
